05d44862b0ccc10bcc8917da7e0f50c3d5034c71
Leander Diaz-Bone <leanderdiazbone@gmail.com>
2023-04-18 16:24:52 -0700

    Added Scripts (no functionality yet) for the game functionality. Which include the general interface functions that the frontend can call.

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,72 @@
+﻿using System;
+public class Game
+{
+	// Variables (Game Board, Players)
+	readonly Player p1;
+	readonly Player p2;
+	private (int,int)[,] board;
+	private Player curMove;
+
+    public Game(int dim)
+	{
+		p1 = new Player(1,this);
+		p2 = new Player(2,this);
+		curMove = p1;
+		board = new (int,int)[dim,dim];
+		for (int i = 0; i < dim; i++) {
+			for (int j = 0; j < dim; j++) {
+				board[i, j] = (-1,0);
+			}
+		}
+		Console.WriteLine("Game was created!");
+	}
+	public bool MakeMove(Player p, (int,int) position, int number) {
+		if (!IsValid(p,position, number))
+		{
+			Console.WriteLine("This move is not valid!");
+			return false;
+		}
+		board[position.Item1, position.Item2] = (number, p.getId());
+		//Change current Score
+		p1.setScore(ComputeScore(p1));
+		p2.setScore(ComputeScore(p2));
+		//Change current Move
+		if (p.Equals(p1))
+		{
+			curMove = p2;
+		}
+		else {
+			curMove = p1;
+		}
+		//Call Functions in UI (add number, change scores, change turn)
+
+		return true;
+	}
+	public Player TerminateGame() {
+		if (p1.getScore() > p2.getScore())
+		{
+			return p1;
+		}
+		else if (p2.getScore() > p1.getScore())
+		{
+			return p2;
+		}
+		else {
+			return null;
+		}
+	}
+	private bool IsValid(Player p, (int, int) position, int number) {
+		if (p.Equals(curMove))
+		{
+			return true;
+		}
+		else {
+			return false;
+		}
+		
+	}
+	private int ComputeScore(Player p) {
+		return 0;
+	}
+}
+
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,23 @@
+﻿using System;
+public class Player
+{
+	readonly int id;
+	int curScore;
+	readonly Game game;
+	public Player(int pid, Game g)
+	{
+		id = pid;
+		game = g;
+		curScore = 0;
+	}
+	public int getId() {
+		return id;
+	}
+	public int getScore() {
+		return curScore;
+	}
+	public void setScore(int score) {
+		curScore = score;
+	}
+}
+
