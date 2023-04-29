3a5b7336837b063e9087d90f6a268df321a459e3
Oagenoagemono <ky46@uw.edu>
2023-04-28 17:07:52 -0700

    Fixed GameScreenAdjuster name

--- Assets/Scripts/GameScreenAdjuster.cs
+++ Assets/Scripts/GameScreenAdjuster.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class ScreenAdjuster : MonoBehaviour
+public class GameScreenAdjuster : MonoBehaviour
 {
     public GameManager gameManager;
     public RectTransform _moveTimer, _gameTimer;
